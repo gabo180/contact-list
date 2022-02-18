455cd7100066127d2c6a7aa78e7d20735eb08cd0
Gabriel Hernandez <gabrieldejesushernandez@gmail.com>
2022-02-17 21:34:27 -0600

    codeAlongContactList

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import React  from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 import ContactTable from './components/ContactTable'
 import ContactForm from './components/ContactForm'
@@ -7,28 +6,61 @@ import ContactModal from './components/ContactModal'
 import { Container, Row, Col } from 'react-bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-function App() {
-  return (
-    <Container fluid>
-      <Row>
-        <Col>
-          <h1 className='text-center'>Contact Application</h1>
-        </Col>
-      </Row>
-      <hr />
-      <Row>
-        <Col sm={8}>
-          <h2>My Contacts</h2>
-          <ContactTable />
-        </Col>
-        <Col sm={4}>
-          <h2>Add New Contact</h2>
-          <ContactForm />
-        </Col>
-      </Row>
-      {/* <ContactModal />*/}
-    </Container>
-  );
+const SERVICE_URL = 'http://contactlist.us-east-1.elasticbeanstalk.com';
+
+class App extends React.Component {
+
+  constructor() {
+    super();
+    this.state = {
+      loading: false,
+      contactData: [
+        {
+          'contactId': 1,
+          'firstName': 'Fake',
+          'lastName': 'Data',
+          'company': 'Unknown Inc.',
+          'phone': '000-0000',
+          'email': 'fakedata@unknown.io'
+        }
+      ]
+    }
+  }
+
+  componentDidMount() {
+    console.log('App is now mounted.')
+    this.setState({ loading: true })
+    console.log('Loading contact data')
+    fetch(SERVICE_URL + '/contacts')
+      .then(data => data.json())
+      .then(data => this.setState(
+        { contactData: data, loading: false },
+      ))
+  }
+
+  render() {
+    return (
+      <Container fluid>
+        <Row>
+          <Col>
+            <h1 className='text-center'>Contact Application</h1>
+          </Col>
+        </Row>
+        <hr />
+        <Row>
+          <Col sm={8}>
+            <h2>My Contacts</h2>
+            <ContactTable contacts={this.state.contactData}/>
+          </Col>
+          <Col sm={4}>
+            <h2>Add New Contact</h2>
+            <ContactForm />
+          </Col>
+        </Row>
+        {/* <ContactModal />*/}
+      </Container>
+    );
+  }
 }
 
 export default App;
--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,5 +1,26 @@
-const ContactForm = () => {
-    return <h1>Contact Form</h1>
+import React from 'react';
+import { Form, Button } from 'react-bootstrap';
+
+class ContactForm extends React.Component {
+    render(){
+        return (
+            <form>
+                <Form.Group controlId='contactFirstName'>
+                    <Form.Label>First Name:</Form.Label>
+                    <Form.Control type='text' placeholder='First Name' name="firstName"/>
+                </Form.Group>
+                <Form.Group controlId='contactLastName'>
+                    <Form.Label>Last Name:</Form.Label>
+                    <Form.Control type='text' placeholder='Last Name' name="lastName"/>
+                </Form.Group>
+                <Form.Group controlId='contactCompany'>
+                    <Form.Label>Company:</Form.Label>
+                    <Form.Control type='text' placeholder='Company' name="company"/>
+                </Form.Group>
+                
+            </form>
+        )
+    }
 }
 
 export default ContactForm;
